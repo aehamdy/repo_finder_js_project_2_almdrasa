2a14fdd33f7c3e7b8a48d35646f04db4c3e6cdbb
aehamdy <ae.hamdy91@gmail.com>
2024-02-13 10:34:59 +0200

    Added functionality to prevent empty requests

--- scripts/elements.js
+++ scripts/elements.js
@@ -2,3 +2,4 @@ export const cardsElement = document.querySelector(".cards");
 export const buttonElement = document.querySelector("button");
 export const searchInput = document.querySelector(".input");
 export const usersInputElement = document.querySelector("input[value='users']");
+export const messageElement = document.querySelector(".message");
--- scripts/performSearch.js
+++ scripts/performSearch.js
@@ -1,10 +1,16 @@
 import setSearchResult from "./setSearchResults.js";
+import setMessage from "./setMessage.js";
 
 const USERS_API = "https://api.github.com/search/users?q=";
 // const USERS_API = "https://api.github.com/search/users?q=apple+org";
 
 const performSearch = (searchTerm, isUserSelected) => {
     const typeQuery = isUserSelected ? "+type:user" : "+type:org";
+
+    if (!searchTerm.trim()) {
+        setMessage("Please fill out the search field")
+        return;
+    }
 
     fetch(`${USERS_API}${searchTerm}${typeQuery}`)
         .then((result) => result.json())
